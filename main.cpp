3c53ac00734bf4691e6f9a053f5a99ddbca28115
reza ahmadi sabzevar <ahmadireza15@gmail.com>
2022-04-08 15:08:56 +0430

    add menu handler functions

--- main.cpp
+++ main.cpp
@@ -27,22 +27,24 @@ using namespace std;
 
 /// functions
 void add_phone(string name, string brand, int stock);
-int search(string name, string brand);
+int  search(string name, string brand);
+void handle_menu(int menu);
 void change_mobile_info();
 void remove(int index);
 void most_sold_item();
 void sort_by_price();
 void sell_a_mobile();
+void cout_menu();
 
 
 int main()
 {
+    cout_menu();
     return 0;
-
 }
 
 /**
- * Add mobile to mobiles array.
+ * Add `phone` to `phones` array.
  *
  * @param name
  * @param brand
@@ -52,13 +54,14 @@ int main()
  */
 void add_phone(string name, string brand, int stock) {
 
+    return cout_menu();
 }
 
 /**
- * Remove mobile with index.
- * then shrink mobiles array
+ * Remove `phone` with index.
+ * then shrink `phones` array
  *
- * @param index of mobiles array.
+ * @param index of `phones` array.
  *
  * @return void
  */
@@ -67,19 +70,19 @@ void remove(int index) {
 }
 
 /**
- * Find mobile with name and brand.
+ * Find `phone` with name and brand.
  *
  * @param name
  * @param brand
  *
- * @return index of `mobile`, or -1 if there was no mobile.
+ * @return index of `phone`, or -1 if there was no `phone`.
  */
 int search(string name, string brand) {
 
 }
 
 /**
- * Sort mobiles array by price.
+ * Sort `phones` array by price.
  * without duplicate array
  */
 void sort_by_price() {
@@ -87,19 +90,17 @@ void sort_by_price() {
 }
 
 /**
- * Change detail of mobile.
+ * Change detail of `phone`.
  *
  * @param name
  * @param brand
- *
- * @return index of `phone`, or -1 if there was no phone.
  */
 void change_mobile_info() {
 
 }
 
 /**
- * Change detail of mobile.
+ * Change detail of `phone`.
  *
  * @param name
  * @param brand
@@ -109,10 +110,94 @@ void sell_a_mobile() {
 }
 
 /**
- * Print mobiles detail sorted by sell.
+ * Print `phones` detail sorted by sell.
  *
  */
 void most_sold_item() {
+
+}
+
+void cout_menu() {
+
+    int menu;
+
+    /// print menu for user
+    cout << "1- Add a phone" << endl;
+    cout << "2- Remove phone" << endl;
+    cout << "3- Sort phones by price" << endl;
+    cout << "4- Search a phone" << endl;
+    cout << "5- Change phone info" << endl;
+    cout << "6- Sell a phone" << endl;
+    cout << "7- Print phones" << endl;
+    cout << "8- Print phones sorted by the most sold to the least" << endl;
+    cout << "9- Quit" << endl;
+    cin >> menu;
+
+    /// prepare for next use
+    cin.clear();
+    std::cin.ignore();
+
+    /// Perform the action associated with each menu item
+    return handle_menu(menu);
+}
+
+/**
+ * Manage menu items
+ * Call suitable function
+ *
+ * @param menu
+ */
+void handle_menu(int menu) {
+
+    switch(menu) {
+        case 1:
+            {
+                /// get phone detail from user
+                string name, brand;
+                int stock;
+
+                cout << "Enter the phone name" << endl;
+                cin >> name;
+
+                cout << "Enter the phone brand" << endl;
+                cin >> brand;
+
+                cout << "Enter the phone stock" << endl;
+                cin >> stock;
+
+                /// call function
+                return add_phone(name, brand, stock);
+            }
+        case 2:
+            // code block
+            break;
+        case 3:
+            // code block
+            break;
+        case 4:
+            // code block
+            break;
+        case 5:
+            // code block
+            break;
+        case 6:
+            // code block
+            break;
+        case 7:
+            // code block
+            break;
+        case 8:
+            // code block
+            break;
+        case 9:
+            cout << "The End ...." << endl;
+            break;
+        default:
+            {
+                cout << "The value entered is incorrect. Please enter the menu number carefully" << endl;
+                return cout_menu();
+            }
+    }
 
 }
 
